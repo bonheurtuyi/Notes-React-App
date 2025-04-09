9ef9313f6d9e4d353531e8471477d9789b07bab6
Bonheur Tuyikunde <btuyikun@andrew.cmu.edu>
2025-04-09 12:15:43 +0200

    Adding props and map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./headers/Header";
 import Footer from "./footers/Footer";
 import Note from "./notes/Note";
+import notes from "./notes/notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(note => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     );
